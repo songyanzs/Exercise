d5370e9477574c43d5f5e2dfc0bc11c63b7efc54
songyan <841722176@qq.com>
2020-11-30 10:36:37 +0800

    commit

--- Lixi/src/demo1/November/LeeLixi/DesExercise.java
+++ Lixi/src/demo1/November/LeeLixi/DesExercise.java
@@ -5,4 +5,17 @@ public class DesExercise {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+     * 输入: [2,3,-2,4]
+     * 输出: 6
+     * 解释: 子数组 [2,3] 有最大乘积 6。
+     * @param nums
+     * @return
+     */
+    public int maxProduct(int[] nums) {
+        int result = nums[0] ;
+        return result;
+    }
 }
