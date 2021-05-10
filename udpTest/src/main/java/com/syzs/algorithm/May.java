1b1ac2dac04ebcecae4e8b840f5cde64e60d7a1c
songyanzs <841722176@qq.com>
2021-05-10 09:07:31 +0800

    commit

--- udpTest/src/main/java/com/syzs/algorithm/May.java
+++ udpTest/src/main/java/com/syzs/algorithm/May.java
@@ -6,4 +6,11 @@ public class May {
         May may = new May();
     }
 
+
+    /**
+     * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+     */
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
 }
